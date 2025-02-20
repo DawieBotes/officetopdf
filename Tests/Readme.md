1cfb7711875f17e7f4fd334a3bf25a1ed0d9fdbb
Dawie Botes <Dawie.Botes@twenty57.com>
2025-02-20 14:03:28 +0200

    removed unrelated test files

--- Tests/Readme.md
+++
@@ -1,1 +0,0 @@
-soffice --accept="socket,host=localhost,port=2002;urp;" --headless
--- Tests/checkscript.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-
-print("Python executable:", sys.executable)
-print("Python path:", sys.path)
-print("Environment variables:", os.environ)
